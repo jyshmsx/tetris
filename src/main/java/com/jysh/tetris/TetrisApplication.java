27db62350bdceb3216f043cb7d8c857d623ed530
jyshmsx <jysh_y@163.com>
2021-07-17 18:39:08 +0800

    添加注释

--- src/main/java/com/jysh/tetris/TetrisApplication.java
+++ src/main/java/com/jysh/tetris/TetrisApplication.java
@@ -10,6 +10,7 @@ import com.jysh.tetris.controller.TetraBoardController;
  *
  *      TODO 目前，对于整体UI的设置是在controller中完成，应当在view中创建一个JFrame完全控制所有的UI绘制，而controller只负责将model和view结合
  *      TODO 同时，目前model中大多数方法都设置为public，应当对此进行控制，只需将控制方块动作、获取板上信息等方法暴露即可
+ *      TODO Tetra.java中注释不符合规范，待修正
  */
 public class TetrisApplication {
     public static void main(String[] args) {
